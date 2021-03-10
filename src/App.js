054f05d0f9c2ecbc4640a7da00c2c06c104e834a
Yoselyn <yseasunr@gmail.com>
2021-03-10 01:07:35 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import {BrowserRouter as Router, Link, NavLink, Redirect, Prompt, BrowserRouter, Switch} from 'react-router-dom';
+import Route from 'react-router-dom/Route';
+import Auth from './Components/Auth';
+import SignUp from './Components/SignUp';
+import AccountPage from './Components/Account';
+import MainNavigation from "./Components/Navigation/MainNavigation";
+
+
+
+class App extends Component {
+render() {
+    return (
+        <BrowserRouter>
+        <React.Fragment>
+        <MainNavigation />
+        <main className="main-content">
+        <Switch>
+            <Redirect from="/" to="/auth" exact/>
+            <Route path="/auth" component={Auth}/>
+            <Route path="/signup" component={SignUp}/>
+            <Route path="/account" component={AccountPage}/>
+            </Switch>
+            </main>
+       
+
+        <Switch>
+            
+        </Switch>
+        </React.Fragment>
+        </BrowserRouter>
+    );
+}
+}
+
+export default App;
---
+++ src/Components/Account.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class AccountPage extends Component {
+    render() {
+        return <h1>Account</h1>;
+    }
+}
+
+export default AccountPage;
