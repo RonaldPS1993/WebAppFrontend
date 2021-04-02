4d97cd5bd78d6950a412ee0631a2c9cf32176b96
Yoselyn <yseasunr@gmail.com>
2021-04-01 21:27:36 -0400

    added account component

--- src/index.js
+++ src/index.js
@@ -2,19 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import { BrowserRouter} from "react-router-dom";
 
 
 
 
 ReactDOM.render(
-  <Router>
+  <BrowserRouter>
 <App/>
-</Router>,
+</BrowserRouter>,
   document.getElementById('root')
 );
 
